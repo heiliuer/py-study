90e7397e031a829b285c668ff4fa9345c727fc70
heiliuer <heiliuer@github.com>
2016-11-18 18:37:24 +0800

    fixed

---
+++ 14遍历指定后缀文件-生成json/config.py
@@ -0,0 +1,11 @@
+CONFIGS = {
+    'FILE_JSON': "files.json",
+
+    'FILE_CONFIG': "folders.ini",
+
+    'FILE_FFMPEG': "../ffmpeg",
+
+    'FILE_NGINX_CONFIG': "nginx-datas.conf",
+
+    'FILE_PATH_IMAGES': "../images"
+}
