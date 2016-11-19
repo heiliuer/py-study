b548aa133630ed95367d60bae92fc166ddf802da
heiliuer <eee>
2016-11-20 02:05:51 +0800

    打开mvn环境变量所在文件夹

--- 15打开where命令所在文件夹/openwhere.py
+++ 15打开where命令所在文件夹/openwhere.py
@@ -14,22 +14,56 @@ def print_help():
 
 
 def open_path_in_explore(path):
-    os.system("explorer.exe \"{}\"".format(path))
+    os.system("explorer.exe  \"{}\"".format(path))
+
+
+def locate_file_in_explore(file):
+    os.system("explorer.exe /select, \"{}\"".format(file))
+
+
+def print_no_newline(str):
+    sys.stdout.write(str)
+    sys.stdout.flush()
+
+
+def read_a_num():
+    try:
+        cha = int(input())
+        return cha
+    except:
+        return -1
+
+
+def open_file_by_num(files, num):
+    if 0 < num <= len(files):
+        # open_path_in_explore(os.path.dirname(files[num - 1]))
+        locate_file_in_explore(files[num - 1])
+        return True
+    print_no_newline('invalid num:')
+    return False
 
 
 def handle(cmd):
     cmd = "where \"{}\"".format(cmd)
 
     try:
-        result = subprocess.check_output(cmd, shell=False, stderr=subprocess.STDOUT).decode()
+        output = subprocess.check_output(cmd, shell=False, stderr=subprocess.STDOUT)
+        result = output.decode("GBK")
     except Exception as e:
-        result = e.output.decode()
+        print(e.output.decode("GBK"))
+        return
 
     if len(result) > 0:
         files = result.split("\r\n")
-        for file in files:
-            if len(file.strip()) > 0:
-                open_path_in_explore(os.path.dirname(file))
+        files = [file for file in files if len(file) > 0]
+        if len(files) > 1:
+            print("select to open:")
+            print('\n'.join((str(index + 1) + '. ' + file) for (index, file) in enumerate(files)))
+            print_no_newline('enter num:')
+            while not open_file_by_num(files, read_a_num()):
+                pass
+        elif len(files) == 1:
+            open_file_by_num(files, 1)
 
 
 if __name__ == '__main__':
