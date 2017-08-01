bf3a04d9a4c3d357faed3d2dc9fc8a4f0cb486da
hao.wang <heiliuer@github.com>
2017-08-01 17:04:10 +0800

    add

--- 16返回时间戳_到剪切板/gettime.py
+++ 16返回时间戳_到剪切板/gettime.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
-from time import gmtime, strftime
 import time
+from time import gmtime, strftime
 
 
 def print_help():
@@ -26,26 +26,42 @@ def read_as_int(arg):
     return None
 
 
-formaters = [("%Y%m%d%H%M%S", "20161213165210"), ("$QQ", "1481648398133 时间戳"), ("%Y%m%d%H%M", "201612131652"),
+formaters = [("%Y%m%d%H%M%S", "20161213165210"), ("%Y_%m%d_%H%M%S", "2016_1213_165210"), ("$QQ", "1481648398133 时间戳"),
+             ("%Y%m%d%H%M", "201612131652"),
              ("%Y-%m-%d-%H-%M", "2016-12-13-16-52")]
 
 
-def print_time(type=0):
+def exec_cmd(cmd):
+    # print({
+    #     cmd
+    # })
+    os.system(cmd)
+
+
+def print_time(type=1):
     if type < 0 or type > len(formaters) - 1:
         print("invalid type")
     else:
         mtime = str(round(time.time() * 1000))
         ftime = strftime(formaters[type][0], gmtime())
-        print(ftime.replace('$QQ', mtime))
+        time_str = ftime.replace('$QQ', mtime)
+        print(time_str)
+        exec_cmd('echo |set /p="' + time_str + '"|clip')
 
 
 def handle(args):
-    if len(args) > 0:
-        type = read_as_int(args[0])
-        if type is None:
-            print_help()
-            return
-        print_time(type - 1)
+    args_length = len(args)
+    if args_length > 0:
+        arg0 = args[0].strip()
+        if arg0 == '':
+            print_time()
+        else:
+            type = read_as_int(arg0)
+            # print({arg0})
+            if type is None:
+                print_help()
+                return
+            print_time(type - 1)
     else:
         print_time()
 
